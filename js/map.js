27922f854bf31baca798101bef1786bf645cea03
QichenJ <QichenJ@JingQichens-MBP.hsd1.ca.comcast.net>
2015-09-29 22:04:42 -0700

    center the map to current location

--- js/map.js
+++ js/map.js
@@ -1,6 +1,21 @@
 var map;
 function initMap(){
-	map = new google.maps.Map(document.getElementById('map'), {		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
+	var options = {
+		zoom : 8,
+		center: {
+			lat: -34.397,
+			lng: 150.644
+		}
+	}
+	var self = this;
+	map = new google.maps.Map(document.getElementById('map'), options);
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			map.setCenter(pos);
+		});
+	}
 }
