c334e6beca4c926479412b726a73124a74ea862f
QichenJ <QichenJ@JingQichens-MBP.hsd1.ca.comcast.net>
2015-10-03 15:06:49 -0700

    add basic html and map function

--- js/map.js
+++ js/map.js
@@ -1,21 +1,98 @@
-var map;
-function initMap(){
-	var options = {
-		zoom : 8,
-		center: {
-			lat: -34.397,
-			lng: 150.644
-		}
-	}
+var meetupURL = 'https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=37.4347&state=CA&text=coding java python web&lon= -121.8950&radius=20&status=upcoming&page=20
+&key=6ab5e2d3c46794b78504857152479b';
+
+/*A Google Map object*/
+var googleMap = function(center, element) {
 	var self = this;
-	map = new google.maps.Map(document.getElementById('map'), options);
+	var options = {
+		zoom: 12,
+		center: center
+	}
+	var map = new google.maps.Map(element, options);
+	return map;
+}
+
+
+//Initial map
+function initMap() {
+	var element = document.getElementById('map');
+	var center = {
+		lat: 37.4347,
+		lng: -121.8950
+	}
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			var pos = {
+			center = {
 				lat: position.coords.latitude,
 				lng: position.coords.longitude
 			};
-			map.setCenter(pos);
 		});
 	}
+	map = new googleMap(center, element);
 }
+
+function fetchData(url) {
+	var data;
+	var result;
+	var res = $.ajax({
+		url: url,
+		type: 'GET',
+		timeout: 5000,
+		dataType: 'jsonp',
+		cache: false
+	});
+	res.done(function(response)) {
+		data = response.results;
+		data.forEach(function(meetup)) {
+			result.push(new Meetup(meetup));
+		}
+	}
+
+	res.fail(function(response, status, error)) {
+		//TODO handle this failure
+
+	}
+}
+
+//TODO create Meetup class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
